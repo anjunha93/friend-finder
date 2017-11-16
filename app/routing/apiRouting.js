2b3ccf5bf9535ef722df8f26a57cea05f1d8b9dc
Jun Ahn <junha.ahn93@gmail.com>
2017-11-16 01:25:20 -0600

    final

---
+++ app/routing/apiRouting.js
@@ -0,0 +1,84 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendData = require("../data/friends.js");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    var score;
+
+    var match = {
+      name: "",
+      photo: "",
+      friendDifference: 10000
+    }
+
+
+    for (var i = 0; i < friendData.length; i++) {
+      var current = friendData[i];
+      score = 0
+
+      for (var j = 0; j < current.scores.length; j++) {
+       score += Math.abs(parseInt(userScores[j])-parseInt(current.scores[j]))
+      }
+
+      if (score <= match.friendDifference){
+        match.name = current.name;
+        match.photo = current.photo;
+        match.friendDifference = score;
+      }
+    }
+
+
+
+
+    friendData.push(req.body);
+    console.log(match);
+    res.json(match);
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    friendData = [];
+
+    console.log(friendData);
+  });
+};
